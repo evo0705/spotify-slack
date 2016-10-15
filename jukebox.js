7ad394b371c83b07661cbb4b2d415045cbe16654
Ravin <ravin@servishero.com>
2016-10-15 15:40:25 +0800

    commit no 37

--- jukebox.js
+++ jukebox.js
@@ -65,17 +65,16 @@ var jukebox = {
 	  	var result = JSON.parse(body);	  		  	
 	    var time = millisToMinutesAndSeconds(result.duration_ms);
     	var html = "--------------NEW TRACK ADDED TO JUKEBOX-------------\n";	      	
-	    html += "*Song*\t\t\t: " + result.name + "\n";
-	    html += "*Album*\t\t: " + result.album.name + "\n";
-	    html += "*Track ID*\t: " + result.id + "\n";
-	    html += "*Duration*\t: " + time + "\n";	    
-	    html += "*Preview*\t: <" + result.preview_url + "|Preview>\n";
-	    html += "*Added By*\t: " + data.name;
-	    return res.send(html);
+	    html += "*Song* : " + result.name + "\n";
+	    html += "*Album* : " + result.album.name + "\n";
+	    html += "*Track ID* : " + result.id + "\n";
+	    html += "*Duration* : " + time + "\n";	    
+	    html += "*Preview* : <" + result.preview_url + "|Preview>\n";
+	    html += "*Added By* : " + data.name;	    
 
 	    spotifyApi.addTracksToPlaylist(data.username, data.playlistId, [result.uri])
-	    .then(function(res) {
-	    	
+	    .then(function(response) {
+	    	return res.send(html);
 	    }, function(err) {
 	      return res.send(err.message);
 	    }); 
